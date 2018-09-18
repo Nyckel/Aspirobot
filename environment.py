7363099aca594d11f7156c0e23e4f55405e6753f
rachelnoireau <rachel.noireau@uha.fr>
2018-09-18 11:39:12 -0400

    debut apparition saleté et bijoux

--- environment.py
+++ environment.py
@@ -1,7 +1,13 @@
+import random
+from room import Room
+
 class Environment:
     GRID_WIDTH = 10
     GRID_HEIGHT = 10
-
+    
+    PROBA_DIRTY = 0.5
+    PROBA_JEWEL = 0.25
+    
     def __init__(self):
         self.grid = [[0 for x in range(self.GRID_WIDTH)] for y in range(self.GRID_HEIGHT)]
 
@@ -12,12 +18,19 @@ class Environment:
         return 0
 
     def should_there_be_a_new_dirty_space(self):
-        return False
+        return (random.random()>PROBA_DIRTY)
 
+    def place_of_new_dirty(self):
+        return int(random.random()*100) #room_number
+    
     def should_there_be_a_new_lost_jewel(self):
-        return False
+        return (random.random()>PROBA_JEWEL)
+		
+    def place_of_new_jewel(self):
+        return int(random.random()*100) #room_number
 
     def generate_dirt(self):
+        
         return True
 
     def generate_jewel(self):
