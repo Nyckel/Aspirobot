47039db634be7379571bbb414f68065af0713f6c
Unknown <mathis.ouarnier@gmail.com>
2018-09-18 22:14:21 -0400

    Display of the elements on the grid

--- environment.py
+++ environment.py
@@ -1,15 +1,30 @@
 import random
 from room import Room
 
+
 class Environment:
+
     GRID_WIDTH = 10
     GRID_HEIGHT = 10
-    
+
     PROBA_DIRTY = 0.5
     PROBA_JEWEL = 0.25
-    
+
     def __init__(self):
-        self.grid = [[0 for x in range(self.GRID_WIDTH)] for y in range(self.GRID_HEIGHT)]
+        self.grid = [[None for x in range(self.GRID_WIDTH)] for y in range(self.GRID_HEIGHT)]
+
+        # Replace by numpy array
+        for row in range(self.GRID_HEIGHT):
+            for col in range(self.GRID_WIDTH):
+                self.grid[row][col] = Room(col, row)
+
+        for i in range(10): # TODO: Replace to have a random number of dirt (in a range...) ?
+            x, y = self.place_of_new_dirt()
+            self.grid[y][x].add_dirt()
+
+        for i in range(2):
+            x, y = self.place_of_new_jewel()
+            self.grid[y][x].add_jewel()
 
     def get_grid(self):
         return self.grid
@@ -18,19 +33,19 @@ class Environment:
         return 0
 
     def should_there_be_a_new_dirty_space(self):
-        return (random.random()>PROBA_DIRTY)
+        return random.random() > self.PROBA_DIRTY
 
-    def place_of_new_dirty(self):
-        return int(random.random()*100) #room_number
-    
+    def place_of_new_dirt(self):
+        return int(random.random()*10), int(random.random()*10)
+
     def should_there_be_a_new_lost_jewel(self):
-        return (random.random()>PROBA_JEWEL)
-		
+        return random.random() > self.PROBA_JEWEL
+
     def place_of_new_jewel(self):
-        return int(random.random()*100) #room_number
+        return int(random.random()*10), int(random.random()*10)
 
     def generate_dirt(self):
-        
+
         return True
 
     def generate_jewel(self):
--- room.py
+++ room.py
@@ -1,9 +1,14 @@
 
-class Room :
+class Room:
 
-	def __init__(self) :
-		(x,y)
-		has_dirt 
-		has_jewel
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.has_dirt = False
+        self.has_jewel = False
 
-		
+    def add_dirt(self):
+        self.has_dirt = True
+
+    def add_jewel(self):
+        self.has_jewel = True
